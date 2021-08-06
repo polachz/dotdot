ba6f7491df9fda7843688a134cc61e2172222276
Zdenek Polach <20874904+polachz@users.noreply.github.com>
2021-08-06 20:00:22 +0200

    Back to nvm 14

--- .myconfig/bin/gatsby-cli_install.sh
+++ .myconfig/bin/gatsby-cli_install.sh
@@ -10,8 +10,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh"  ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion"  ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-nvm install 16
-nvm use 16
+nvm install 14
+nvm use 14
 
 npm install react react-dom
 npm install -g gatsby-cli
