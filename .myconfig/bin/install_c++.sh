14d77f97f43b1bc73f6e08582cc3af48ab1b6bbb
polachz <20874904+polachz@users.noreply.github.com>
2023-02-24 07:39:04 +0100

    ModifyC++ install

--- .myconfig/bin/install_c++.sh
+++ .myconfig/bin/install_c++.sh
@@ -1,4 +1,5 @@
 sudo dnf -y install libstdc++
+sudo dnf -y groupinstall "Development Tools"
 sudo dnf -y groupinstall "C Development Tools and Libraries"
 sudo dnf -y install patch patchutils
 #sudo dnf groupinstall "Development Libraries"
