71c1ec33d42f3f594fc3b2a802a8aeeca5c9f05f
polachz <20874904+polachz@users.noreply.github.com>
2023-08-17 20:42:00 +0200

    new blog

--- .myconfig/bin/install_blog.sh
+++ .myconfig/bin/install_blog.sh
@@ -5,24 +5,13 @@ if [ -d "$HOME/blog" ]; then
     exit 1
 fi
 
-if [ ! -d "$HOME/.nvm" ]; then
-     echo "Installing NVM and gatsby-cli..."
-     #Instal nvm ang gatsby cli - we have this in the path in my environment
-     #then call it directly without any magic
-     install_gatsby-cli.sh
-fi
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh"  ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion"  ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+sudo install hugo
 
 #now clone blog repository
 git clone --recurse-submodules git@github.com:polachz/blog.git "$HOME/blog"
 
 #switch to blog directory and install gatsby
 cd "$HOME/blog"
-
-npm install gatsby
 
 detect_wsl_subsystem
 
@@ -35,5 +24,5 @@ fi
 # gatsby develop
 echo "to develop the blog, run:"
 echo "cd ~/blog"
-echo "gatsby develop"
+echo "hugo server --disableFastRender"
 
